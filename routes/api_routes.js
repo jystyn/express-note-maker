1e8f78f43c7b54cf04b25dec6e34b2d7b283cc0b
joostyn <justyn.helgeson@gmail.com>
2022-07-27 10:03:55 -0500

    Created delete request

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,6 +1,7 @@
 const notes_router = require('express').Router();
 const fs = require('fs');
 const path = require('path');
+const uuid = require('uuid').v4;
 const db_path = path.join(__dirname, '../db/db.json');
 
 function getNoteData() {
@@ -19,6 +20,9 @@ notes_router.post('/notes', (request, response) => {
         .then(note_data => {
             const new_note = request.body;
 
+            //Adds id to note so we can target them for delete
+            new_note.id = uuid().slice(0, 4);
+
             note_data.push(new_note);
 
             fs.promises.writeFile(db_path, JSON.stringify(note_data, null, 2))
@@ -27,8 +31,20 @@ notes_router.post('/notes', (request, response) => {
         })
 });
 
-notes_router.delete('/notes', (request, respons) => {
-    // getNoteData()
+notes_router.delete('/notes/:id', (request, response) => {
+    getNoteData()
+        .then(note_data => {
+            const id = request.params.id;
+            const obj = note_data.find(note_data => note_data.id === id);
+            const index = note_data.indexOf(obj);
+
+            note_data.splice(index, 1);
+
+            fs.promises.writeFile(db_path, JSON.stringify(note_data, null, 2))
+                .then(() => response.json(note_data))
+                .catch(err => console.log(err));
+        });
+
 })
 
 module.exports = notes_router;
